14eb89e90733d22854de3a0ec4494d2dd07894f3
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:17:01 +0430

    init

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title, info}) => {
-  return <h2>question component</h2>;
+  return <article>
+    
+  </article>
 };
 
 export default Question;
