b3ea7368c3e706bd4f998f517b24279e4bb7c0f5
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:14:47 +0430

    init

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <h3>questions and answers about login</h3>
       <section className='info'>
           {data.map((qustion) => {
-            
-            return <SingleQuestion />
+            const {id, title, info} = questions
+            return <SingleQuestion key={id} />
           })}
       </section>
     </div>
