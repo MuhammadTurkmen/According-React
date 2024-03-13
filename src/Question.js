bb22381b72ecc7196b245eb09ef2d327103034c1
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:22:04 +0430

    init

--- src/Question.js
+++ src/Question.js
@@ -5,7 +5,7 @@ const Question = ({title, info}) => {
   return <article className='question'>
     <header>
       <h4>{title}</h4>
-      <button className='btn'>
+      <button className='btn' onClick={() => }>
         btn
       </button>
     </header>
