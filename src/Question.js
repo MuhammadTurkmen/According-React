3e4516d386f50d04bfbaf190db10f9a9709a7202
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-03-13 10:20:38 +0430

    init

--- src/Question.js
+++ src/Question.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({title, info}) => {
+  const [showInfo, setShowInfo] = useState(false)
   return <article className='question'>
     <header>
       <h4>{title}</h4>
